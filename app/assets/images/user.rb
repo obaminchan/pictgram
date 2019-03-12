2d0d2f2b4820e62c1d3791819a3a02047e5534ac
obaminchan <manucasol.0811@gmail.com>
2019-03-12 15:59:26 +0900

    task submit

---
+++ app/assets/images/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+
+
+  has_secure_password
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
+
+  has_many :topics
+end
